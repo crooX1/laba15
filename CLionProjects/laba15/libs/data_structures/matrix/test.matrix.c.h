181fd90313ee1ac75b5b8f914b6f96bf35228635
crooX <vladickkos20015@mail.ru>
2024-05-22 11:38:32 +0300

    test.matrix.c

---
+++ CLionProjects/laba15/libs/data_structures/matrix/test.matrix.c.h
@@ -0,0 +1,88 @@
+#ifndef LABA15_TEST_MATRIX_C_H
+#define LABA15_TEST_MATRIX_C_H
+
+#include "matrix.h"
+
+int getSum(int *a, int n);
+
+void test_getMemMatrix1();
+
+void test_getMemMatrix2();
+
+void test_getMemMatrix();
+
+void test_getMemArrayOfMatrices1();
+
+void test_getMemArrayOfMatrices2();
+
+void test_getMemArrayOfMatrices();
+
+void test_swapRows1();
+
+void test_swapRows2();
+
+void test_swapRows();
+
+void test_swapColumns_oneColumn();
+
+void test_swapColumns_twoColumns();
+
+void test_swapColumns();
+
+void test_insertionSortRowsMatrixByRowCriteria1();
+
+void test_insertionSortRowsMatrixByRowCriteria2();
+
+void test_insertionSortRowsMatrixByRowCriteria();
+
+void test_insertionSortColsMatrixByColCriteria1();
+
+void test_insertionSortColsMatrixByColCriteria2();
+
+void test_insertionSortColsMatrixByColCriteria();
+
+void test_isSquareMatrix();
+
+void test_isSquareMatrix2();
+
+void test_isSquareMatrix();
+
+void test_areTwoMatricesEqual1();
+
+void test_areTwoMatricesEqual2();
+
+void test_areTwoMatricesEqual();
+
+void test_isEMatrix1();
+
+void test_isEMatrix2();
+
+void test_isEMatrix();
+
+void test_isSymmetricMatrix1();
+
+void test_isSymmetricMatrix2();
+
+void test_isSymmetricMatrix();
+
+void test_transposeSquareMatrix1();
+
+void test_transposeSquareMatrix2();
+
+void test_transposeSquareMatrix();
+
+void test_getMinValuePos1();
+
+void test_getMinValuePos2();
+
+void test_getMinValuePos();
+
+void test_getMaxValuePos1();
+
+void test_getMaxValuePos2();
+
+void test_getMaxValuePos();
+
+void test();
+
+#endif //LABA15_TEST_MATRIX_C_H
--- CLionProjects/laba15/main.c
+++ CLionProjects/laba15/main.c
@@ -1,1 +1,7 @@
+#include "libs/data_structures/matrix/test.matrix.c.h"
 
+int main(){
+    test();
+
+    return 0;
+}
